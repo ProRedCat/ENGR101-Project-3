b42fe30b4c0e076e84d68de8129576604e882eb6
ProRedCat <43058406+ProRedCat@users.noreply.github.com>
2020-06-01 20:59:58 +1200

    AVC_Robot project

---
+++ AVC_Robot/cmake-build-debug/CMakeFiles/AVC_Robot.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/Reilly/Documents/School/Victoria University/ENGR101/Project 3/AVC_Robot/robot.cpp" "C:/Users/Reilly/Documents/School/Victoria University/ENGR101/Project 3/AVC_Robot/cmake-build-debug/CMakeFiles/AVC_Robot.dir/robot.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "SFML_STATIC"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "C:/Users/Reilly/Documents/mingw64/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ AVC_Robot/cmake-build-debug/CMakeFiles/AVC_Robot.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "AVC_Robot.exe"
+  "AVC_Robot.exe.manifest"
+  "AVC_Robot.pdb"
+  "CMakeFiles/AVC_Robot.dir/robot.cpp.obj"
+  "libAVC_Robot.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AVC_Robot.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
