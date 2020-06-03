4bae7c8772f670da48603803647cbeb23a2fb9a4
Reilly <reilly.oldham@gmail.com>
2020-06-04 01:24:46 +1200

    Better core version
    Fixed the jittery nature of the previous versions core. Realised the view of the robot is actually flipped, so fixed that, and the robot is also no longer offcenter

--- AVC_Robot/robot.cpp
+++ AVC_Robot/robot.cpp
@@ -1,58 +1,50 @@
 #include "robot.hpp"
-#include <cmath>
-
-using namespace std;
-
-double mapValue(double x, double in_min, double in_max, double out_min, double out_max) {
-    return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
-}
 
 int main() {
-    if (initClientRobot() != 0) {
+    if (initClientRobot() != 0) { //Check if the robot was not initialised
         std::cout << " Error initializing robot" << std::endl;
     }
+
+    //Velocities for the robot wheels
     double vLeft = 0;
     double vRight = 0;
 
-    while (true) {
-        takePicture();
+    while (true) { //Loop continuously
+        takePicture(); //Take a picture to update the robots view
 
-        double totalX = 0;
+        //Variables for finding average y position of the line
         double totalY = 0;
         int pixelCount = 0;
-        for (int x = 0; x < cameraView.width; x++) {
-            for (int y = 0; y < cameraView.height; y++) {
-                if(get_pixel(cameraView, y, x, 3) > 230){
-                    totalX += y;
-                    totalY += x;
-                    pixelCount++;
+
+        //Image is rotated so y is width and x is height
+        for (int y = 0; y < cameraView.width; y++) { //Loop through all y pixels
+            for (int x = 0; x < cameraView.height; x++) { //Loop through all x pixes
+                if (get_pixel(cameraView, x, y, 3) > 230) {
+                    totalY += y; //Add the y value for finding average y position
+                    pixelCount++; //Increase pixel count for finding average y position
                 }
             }
         }
 
-        double xPos = totalX/pixelCount;
-        double yPos = totalY/pixelCount;
+        double yPos = totalY / pixelCount; //Calculate average y position of line
 
-        cout<<xPos<<" xPos; "<<yPos<<" yPos"<<endl;
-
-        if((yPos > 50 && (yPos - 5 < 50)) || (yPos < 50 && (yPos + 5 > 50))){
+        if ((yPos > 75 && (yPos - 10 < 75)) || (yPos < 75 && (yPos + 10 > 75))) { //Check if the robot is pointing generally at the line
             vLeft = 40;
             vRight = 40;
-        }
-        else if(yPos < 50){
-            vLeft = 0;
-            vRight = 5;
-        }
-        else if(yPos > 50){
+        } else if (yPos < 75) { //If the robot if pointing left of the line turn right
             vLeft = 5;
-            vRight = 0;
+            vRight = 10;
+        } else if (yPos > 75) { //If the robot is pointing right of the line turn left
+            vLeft = 10;
+            vRight = 5;
+        } else { //If it can't find any rotate on the spot in hopes of finding it
+            vLeft = 0;
+            vRight = 40;
         }
 
-        setMotors(vLeft, vRight);
-        //std::cout << " vLeft=" << vLeft << "  vRight=" << vRight << std::endl;
-        usleep(10000);
-    } //while
-
-} // main
+        setMotors(vLeft, vRight); //Set the velocity of the motors
+        usleep(10000); //Sleep for a time as to not spam the server
+    }
+}
 
 
