2a0db7b745f3cca6f440d53e628578c6fb76a4be
Reilly <reilly.oldham@gmail.com>
2020-06-04 00:53:40 +1200

    Core robot
    Core robot for project. Note: movement is jittery, could be made smoother

--- AVC_Robot/robot.cpp
+++ AVC_Robot/robot.cpp
@@ -1,17 +1,58 @@
 #include "robot.hpp"
+#include <cmath>
 
-int main(){
-	if (initClientRobot() !=0){
-		std::cout<<" Error initializing robot"<<std::endl;
-	}
-    double vLeft = 40.0;
-    double vRight = 30.0;
-    takePicture();
-    SavePPMFile("i0.ppm",cameraView);
-    while(true){
-      setMotors(vLeft,vRight);   
-      std::cout<<" vLeft="<<vLeft<<"  vRight="<<vRight<<std::endl;
-       usleep(10000);
-  } //while
+using namespace std;
+
+double mapValue(double x, double in_min, double in_max, double out_min, double out_max) {
+    return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
+int main() {
+    if (initClientRobot() != 0) {
+        std::cout << " Error initializing robot" << std::endl;
+    }
+    double vLeft = 0;
+    double vRight = 0;
+
+    while (true) {
+        takePicture();
+
+        double totalX = 0;
+        double totalY = 0;
+        int pixelCount = 0;
+        for (int x = 0; x < cameraView.width; x++) {
+            for (int y = 0; y < cameraView.height; y++) {
+                if(get_pixel(cameraView, y, x, 3) > 230){
+                    totalX += y;
+                    totalY += x;
+                    pixelCount++;
+                }
+            }
+        }
+
+        double xPos = totalX/pixelCount;
+        double yPos = totalY/pixelCount;
+
+        cout<<xPos<<" xPos; "<<yPos<<" yPos"<<endl;
+
+        if((yPos > 50 && (yPos - 5 < 50)) || (yPos < 50 && (yPos + 5 > 50))){
+            vLeft = 40;
+            vRight = 40;
+        }
+        else if(yPos < 50){
+            vLeft = 0;
+            vRight = 5;
+        }
+        else if(yPos > 50){
+            vLeft = 5;
+            vRight = 0;
+        }
+
+        setMotors(vLeft, vRight);
+        //std::cout << " vLeft=" << vLeft << "  vRight=" << vRight << std::endl;
+        usleep(10000);
+    } //while
 
 } // main
+
+
